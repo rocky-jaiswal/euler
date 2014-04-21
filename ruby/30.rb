011c7fe83c50751febef0098878cff7c03e6dc6c
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2014-04-21 20:33:45 +0200

    Update 30.rb

--- ruby/30.rb
+++ ruby/30.rb
@@ -1,7 +1,6 @@
-max = (9 ** 5) * 5
+max = (9 ** 5) * 7
 arr = (2..max).select do |n|
-  sum = 0
-  sum_4 = n.to_s.split(//).inject(sum){|sum, e| sum + (e.to_i ** 5)}
+  sum_4 = n.to_s.split(//).inject(0){|sum, e| sum + (e.to_i ** 5)}
   sum_4 == n
 end
 
